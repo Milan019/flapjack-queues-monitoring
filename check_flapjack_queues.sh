79c9374dcb3d4cb42660e66bdf013742ff06210a
Zoran Stefanovic <zorans@vast.com>
2016-10-19 23:51:04 +0200

    added declare -A RESULTS

--- check_flapjack_queues.sh
+++ check_flapjack_queues.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# Author: Zoran Stefanovic
+# Date: 2016-10-19
 # Description: Flapjack queues check
+# Ver: 0.2
 
-set -x; # Enable this to enable debug mode.
+#set -x; # Enable this to enable debug mode.
 
-MAILIST="your@email.com"
+MAILIST="user@domain.com"
 
 CRITICAL_events=3000
 CRITICAL_notifications=1000
@@ -13,46 +16,59 @@ WARNING_events=2000
 WARNING_notifications=500
 WARNING_email_notifications=100
 
-QUEUES=( 'events' 'notifications' 'email_notifications' )
+declare -A RESULTS
 
-i=0
-for QUEUE in "${QUEUES[@]}"; do
+RESPONSE=$(curl -sS --connect-timeout 10 --max-time 20 sensu.domain.com:13081/queues 2>&1)
 
-	case ${QUEUE} in
-		events )
-			CRITICAL=${CRITICAL_events}
-			WARNING=${WARNING_events}
-		;;
-		notifications )
-			CRITICAL=${CRITICAL_notifications}
-			WARNING=${WARNING_notifications}
-		;;
-		email_notifications )
-			CRITICAL=${CRITICAL_email_notifications}
-			WARNING=${WARNING_email_notifications}
-		;;
-		* )
-			exit
-		;;
-	esac
+if [[ ${?} != 0 ]]; then
+	RESULT[0]=`echo "WARNING - connection error: ${RESPONSE}"`
+else
+	RESULTS=(
+	  ['events']=$(echo ${RESPONSE} | jq '.flapjack.queues.events')
+	  ['notifications']=$(echo ${RESPONSE} | jq '.flapjack.queues.notifications')
+	  ['email_notifications']=$(echo ${RESPONSE} | jq '.flapjack.queues.email_notifications')
+	)
 
-	TEST=`curl -sS --connect-timeout 10 --max-time 20 sensu.domain.com:13081/queues | jq .flapjack.queues.${QUEUE}`
-	if [ -z ${TEST} ] ; then
-		RESULT[${i}]=`echo "WARNING - timeout connection error: ${TEST};"`
-	elif [ ${TEST} -gt ${CRITICAL} ] ; then
-		RESULT[${i}]=`echo "CRITICAL - flapjack.queues.${QUEUE} = ${TEST} > ${CRITICAL};"`
-	elif [ ${TEST} -gt ${WARNING} ] ; then
-		RESULT[${i}]=`echo "WARNING - flapjack.queues.${QUEUE} = ${TEST} > ${WARNING};"`
-	else
-		RESULT[${i}]=`echo "OK - flapjack.queues.${QUEUE} = ${TEST};"`
-	fi
-	i=`expr $i + 1`
+	QUEUES=( 'events' 'notifications' 'email_notifications' )
 
-done
+	i=0
+	for QUEUE in "${QUEUES[@]}"; do
+
+		case ${QUEUE} in
+			events )
+				CRITICAL=${CRITICAL_events}
+				WARNING=${WARNING_events}
+				TEST=${RESULTS['events']}
+			;;
+			notifications )
+				CRITICAL=${CRITICAL_notifications}
+				WARNING=${WARNING_notifications}
+				TEST=${RESULTS['notifications']}
+			;;
+			email_notifications )
+				CRITICAL=${CRITICAL_email_notifications}
+				WARNING=${WARNING_email_notifications}
+				TEST=${RESULTS['email_notifications']}
+			;;
+			* )
+				exit
+			;;
+		esac
+
+		if [ ${TEST} -gt ${CRITICAL} ] ; then
+			RESULT[${i}]=`echo "CRITICAL - flapjack.queues.${QUEUE} = ${TEST} > ${CRITICAL};"`
+		elif [ ${TEST} -gt ${WARNING} ] ; then
+			RESULT[${i}]=`echo "WARNING - flapjack.queues.${QUEUE} = ${TEST} > ${WARNING};"`
+		else
+			RESULT[${i}]=`echo "OK - flapjack.queues.${QUEUE} = ${TEST};"`
+		fi
+		i=`expr $i + 1`
+	done
+fi
 
 sendMailAlert ()
 {
-        echo "$1" | mail -r "`hostname`" -s "$2" $MAILIST
+	echo "$1" | mail -s "$2" $MAILIST
 }
 
 if [[ `echo ${RESULT[*]} | grep "CRITICAL" | wc -l` > 0 ]] ; then 
